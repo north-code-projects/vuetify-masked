7d8460bc47d6ae21146d76c82fd5ed1316bc9b2d
Kim Mannstedt <k.mannstedt@gmail.com>
2021-07-04 09:08:07 +0200

    added eslint config and refactored code to fulfill rules

--- src/utils/validator.js
+++ src/utils/validator.js
@@ -1,5 +1,5 @@
-export function charIsValid(c, mask, typeIsText) {
-  if(typeIsText && (mask.onlyAlphabetical && !isNaN(parseFloat(c))) || (mask.onlyDigit && isNaN(parseFloat(c)))) {
+export function charIsValid (c, mask, typeIsText) {
+  if (typeIsText && (mask.onlyAlphabetical && !isNaN(parseFloat(c))) || mask.onlyDigit && isNaN(parseFloat(c))) {
     return false
   } else {
     return true
